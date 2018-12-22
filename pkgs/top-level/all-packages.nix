e7e397c06c7b1958e184d7378f4172cf079d33dc
Bruno Vieira <mail@bmpvieira.com>
2018-12-22 16:50:42 +0000

    Add Node.js to top-level Nix

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -23,11 +23,19 @@ let
   pythonPackages = python37Packages;
   python = pkgs.python3;
 
+  nodePackages_10_x = callPackage ../development/node-packages/default-v10.nix {
+    nodejs = pkgs.nodejs-10_x;
+  };
+  nodePackages = nodePackages_10_x;
+  nodejs = pkgs.nodejs-10_x;
+
   languagesModules = {
     rPackages = rPackages;
     python = python;
     pythonPackages = pythonPackages;
     python37Packages = python37Packages;
+    nodejs = nodejs;
+    nodePackages = nodePackages;
   };
 
   customPkgs = {
