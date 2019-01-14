452dad8b9827c7eb99143e347e6ca2868fd28ffe
Nokome Bentley <nokome@stenci.la>
2019-01-14 14:39:16 +1300

    refactor: create nix folder

---
+++ nix/pkgs/development/r-modules/README.md
@@ -0,0 +1,4 @@
+R script to
+Based on https://github.com/NixOS/nixpkgs/blob/master/pkgs/development/r-modules/generate-r-packages.R
+
+`default.nix` and `generate-r-packages.R` are from the upstream folder (pkgs/development/r-modules)[https://github.com/NixOS/nixpkgs/tree/master/pkgs/development/r-modules]
---
+++ nix/pkgs/development/r-modules/generate.sh
@@ -0,0 +1,11 @@
+#! /usr/bin/env nix-shell
+#! nix-shell -i bash -p wget parallel R rPackages.data_table
+#! nix-shell -I nixpkgs=../../..
+
+# Generate new `*-packages.nix` files for each R package repository
+# in the following list. 
+# The renaming step is required because `generate-r-packages.R <repo>` reads `<repo>-packages.nix`
+for repo in cran bioc bioc-annotation bioc-experiment; do
+    Rscript generate-r-packages.R $repo > $repo-packages.nix.new
+    [ -s $repo-packages.nix.new ] && mv $repo-packages.nix.new $repo-packages.nix
+done
