fb13e0d4847737b4e64eae19ea87582bbe9d1def
Nokome Bentley <nokome@stenci.la>
2019-01-30 16:50:26 +1300

    fix(serve): use xterm from unpkg instead of node_modules

--- src/serve.ts
+++ src/serve.ts
@@ -9,34 +9,46 @@ const app = express()
 const expressWs = require('express-ws')(app)
 
 // Serve static assets from ./static
-app.use(express.static(path.join(path.dirname(__dirname), 'static')))
+app.use(express.static(path.join(__dirname, 'static')))
 
 // Instantiate shell and set up data handlers
-expressWs.app.ws('/shell', async (ws: any, req: any) => {
-  // Create streams that pipe between the Websocket and
-  // the pseudo terminal
+expressWs.app.ws('/shell', async (ws: any, req: express.Request) => {
+  try {
+    // Create streams that pipe between the Websocket and
+    // the pseudo terminal
 
-  // A pseudo stdin that receives data from the Websocket
-  const stdin = new stream.PassThrough()
-  ws.on('message', (msg: any) => {
-    stdin.write(msg)
-  })
+    // A pseudo stdin that receives data from the Websocket
+    const stdin = new stream.PassThrough()
+    ws.on('message', (msg: any) => {
+      stdin.write(msg)
+    })
 
-  // A pseudo stdout that writes data to the Websocket
-  const stdout = new stream.Writable({
-    write (chunk: Buffer, encoding: any, callback: any) {
-      ws.send(chunk)
-      callback()
-    }
-  })
+    // A pseudo stdout that writes data to the Websocket
+    const stdout = new stream.Writable({
+      write (chunk: Buffer, encoding: any, callback: any) {
+        ws.send(chunk)
+        callback()
+      }
+    })
 
-  let env = new Environment('multi-mega')
+    let env = new Environment('multi-mega')
 
-  const sessionParameters = new SessionParameters()
-  sessionParameters.platform = Platform.DOCKER
-  sessionParameters.stdin = stdin
-  sessionParameters.stdout = stdout
-  await env.enter(sessionParameters)
+    const sessionParameters = new SessionParameters()
+    sessionParameters.platform = Platform.DOCKER
+    sessionParameters.stdin = stdin
+    sessionParameters.stdout = stdout
+    await env.enter(sessionParameters)
+  } catch (error) {
+    console.error(error)
+  }
+})
+
+// Error handling middleware
+app.use((error: Error, req: express.Request, res: express.Response, next: any) => {
+  console.error(error.stack)
+  res.status(500)
+  res.render('error', { error })
+  next(error)
 })
 
 app.listen(3000)
