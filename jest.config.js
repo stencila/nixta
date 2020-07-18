8e71247f58984a263ca7e972530bff367272a644
Nokome Bentley <nokome@stenci.la>
2020-07-18 17:46:19 +1200

    refactor(*): Add dev-config; remove serving functionality

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,6 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  testMatch: [
-    "**/tests/**/*.test.ts"
-  ],
-  coveragePathIgnorePatterns: [
-  ]
+  testMatch: ['**/tests/**/*.test.ts'],
+  coveragePathIgnorePatterns: [],
 }
